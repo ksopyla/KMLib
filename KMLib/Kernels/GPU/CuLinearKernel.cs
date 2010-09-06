fdf6de252ab22bed6fe98825455653087d1f5325
ksirg <ksirg@wp.pl>
2010-09-06 11:57:03 +0200

    finish cuda SVM linear kernel

--- KMLib/Kernels/GPU/CuLinearKernel.cs
+++ KMLib/Kernels/GPU/CuLinearKernel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using dnAnalytics.LinearAlgebra;
 using GASS.CUDA;
 using GASS.CUDA.Types;
+using System.IO;
 
 
 
@@ -14,12 +15,18 @@ namespace KMLib.Kernels.GPU
     class CuLinearKernel : VectorKernel<SparseVector> , IDisposable
     {
 
+        private const string cudaModuleName = "structKernel.cubin";
+        const string cudaKernelName = "spmv_csr_vector_kernel";
+        const string cudaTextureRefName="texRef";
+
+
         /// <summary>
         /// linear kernel for normal product
         /// </summary>
         private LinearKernel linKernel;
 
-
+        #region cuda types
+        
         /// <summary>
         /// Cuda .net class for cuda opeation
         /// </summary>
@@ -59,6 +66,26 @@ namespace KMLib.Kernels.GPU
         /// cuda reference to texture, 
         /// </summary>
         CUtexref cuTexRef;
+
+        /// <summary>
+        /// cuda array neded for copy vector to texture
+        /// </summary>
+        CUarray cuArray;
+        #endregion
+
+        /// <summary>
+        /// vector for computing kernel product with other vectors
+        /// </summary>
+        /// <remarks>all the time this vector will be modified and copied to cuda array</remarks>
+        float[] mainVector;
+
+
+        /// <summary>
+        /// Array for dot product results
+        /// </summary>
+        float[] productResults;
+
+
         /// <summary>
         /// average vector lenght, its only a heuristic
         /// </summary>
@@ -107,17 +134,32 @@ namespace KMLib.Kernels.GPU
 
             //cuda calculation
 
+            SparseVector mainVec = problemElements[element1];
 
-            return base.AllProducts(element1);
+
+            Array.Clear(mainVector, 0, mainVector.Length);
+            for (int j = 0; j < mainVec.mValueCount; j++)
+            {
+                int idx = mainVec.mIndices[j];
+                float val =(float) mainVec.mValues[j];
+                mainVector[idx] = val;
+            }
+
+
+            //copy to texture
+            cuda.CopyHostToArray(cuArray, mainVector, 0);
+            cuda.Launch(cuFunc, blocksPerGrid, 1);
+
+            cuda.SynchronizeContext();
+            cuda.CopyDeviceToHost(outputPtr, productResults);
+
+            return productResults;
         }
 
 
         public override void Init()
         {
             base.Init();
-            
-          
-
 
             //transform elements to specific array format -> CSR http://en.wikipedia.org/wiki/Sparse_matrix#Compressed_sparse_row_.28CSR_or_CRS.29
             //
@@ -141,15 +183,15 @@ namespace KMLib.Kernels.GPU
             for (int i = 0; i < problemElements.Length; i++)
             {
                 var vec = problemElements[i];
-                
-                vecValsL.AddRange(vec.mValues);
-                
+
+                vecValsL.AddRange(Array.ConvertAll<double, float>(vec.mValues, Convert.ToSingle));
+
                 vecIdxL.AddRange(vec.mIndices);
-                
+
                 vecLenghtL.Add(vecStartIdx);
                 vecStartIdx += vec.mValueCount;
-
             }
+
             //for last index
             vecLenghtL.Add(vecStartIdx);
 
@@ -172,11 +214,11 @@ namespace KMLib.Kernels.GPU
             vecLenghtPtr = cuda.CopyHostToDevice(vecLenght);
 
             //alocate memory on device
-            float[] output = new float[problemElements.Length];
-            outputPtr = cuda.Allocate(output);
+            productResults = new float[problemElements.Length];
+            outputPtr = cuda.Allocate(productResults);
 
-            cuModule = cuda.LoadModule(Path.Combine(Environment.CurrentDirectory, "structKernel.cubin"));
-            cuFunc = cuda.GetModuleFunction("spmv_csr_vector_kernel");
+            cuModule = cuda.LoadModule(Path.Combine(Environment.CurrentDirectory, cudaModuleName));
+            cuFunc = cuda.GetModuleFunction(cudaKernelName);
             
             #endregion
 
@@ -198,14 +240,25 @@ namespace KMLib.Kernels.GPU
             cuda.SetParameter(cuFunc, offset, (uint)problemElements.Length);
             offset += sizeof(int);
 
-            //this parameter is not nessesary
+            //todo: this parameter is not nessesary
             cuda.SetParameter(cuFunc, offset, (uint)vecStartIdx);
             offset += sizeof(int);
             cuda.SetParameterSize(cuFunc, (uint)offset);
 
             #endregion
-            CUtexref cuTexRef = cuda.GetModuleTexture(cuModule, "texRef");
+          
+            //get reference to cuda texture
+            CUtexref cuTexRef = cuda.GetModuleTexture(cuModule, cudaTextureRefName);
             cuda.SetTextureFlags(cuTexRef, 0);
+
+            //allocate memory for main vector, size of this vector is the same as dimenson, so many 
+            //indexes will be zero, but cuda computation is faster
+            mainVector = new float[problemElements[0].Count];
+
+            //create cuda array and bind to texture
+            cuArray = cuda.CreateArray(mainVector);
+            cuda.SetTextureArray(cuTexRef, cuArray);
+
         }
 
 
@@ -215,14 +268,13 @@ namespace KMLib.Kernels.GPU
         {
             if (cuda != null)
             {
-
-                //todo free all resources
+                //free all resources
                 cuda.Free(valsPtr);
                 cuda.Free(idxPtr);
                 cuda.Free(outputPtr);
                 cuda.Free(vecLenghtPtr);
-                
-                cuda.DestroyArray(cuArr);
+
+                cuda.DestroyArray(cuArray);
                
                 cuda.DestroyTexture(cuTexRef);
 
