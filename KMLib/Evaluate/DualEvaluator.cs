9a649b1eb4f6f95614caaf7e4be742501afc684b
fivitti <fivitti@gmail.com>
2014-11-30 13:21:01 +0100

    Remove commented code from DualEvaluator

--- KMLib/Evaluate/DualEvaluator.cs
+++ KMLib/Evaluate/DualEvaluator.cs
@@ -16,54 +16,6 @@ namespace KMLib.Evaluate
     /// </remarks>
     public class DualEvaluator<TProblemElement> : Evaluator<TProblemElement>
     {
-        
-        //#region IEvaluator<SparseVector> Members
-
-        ///// <summary>
-        ///// Predicts the class of specified elements.
-        ///// </summary>
-        ///// <remarks>Computes this on many CPU cores</remarks>
-        ///// <param name="elements">Array with elements to predict.</param>
-        ///// <returns>array with predicted class for each element</returns>
-        //public override float[] Predict(SparseVector[] elements)
-        //{
-        //    float[] predictions = new float[elements.Length];
-
-        //    Parallel.For(0, elements.Length, i =>
-        //    {
-
-        //        predictions[i] = Predict(elements[i]);
-        //    });
-
-        //    return predictions;
-        //}
-
-        ///// <summary>
-        ///// Predicts the specified element.
-        ///// </summary>
-        ///// <param name="element">The element.</param>
-        ///// <returns>predicted class</returns>
-        //public override float Predict(SparseVector element)
-        //{
-        //    float sum = 0;
-
-        //    int index = -1;
-
-        //    for (int k = 0; k < TrainedModel.SupportElementsIndexes.Length; k++)
-        //    {
-        //        index = TrainedModel.SupportElementsIndexes[k];
-        //        sum += TrainedModel.Alpha[index] * TrainningProblem.Labels[index] *
-        //                            Kernel.Product(TrainningProblem.Elements[index], element);
-        //    }
-
-        //    sum -= TrainedModel.Rho;
-
-        //    float ret = sum > 0 ? 1 : -1;
-        //    return ret;
-        //}
-
-        //#endregion
-
         public override void Init()
         {
             //we don't have to initialized anything
@@ -83,12 +35,6 @@ namespace KMLib.Evaluate
 
             float[] predictions = new float[elements.Length];
 
-            //Parallel.For(0, elements.Length, i =>
-            //{
-
-            //    predictions[i] = Predict(elements[i]);
-            //});
-
             for (int i = 0; i < elements.Length; i++)
             {
                 predictions[i] = Predict(elements[i]);
@@ -96,29 +42,5 @@ namespace KMLib.Evaluate
 
             return predictions;
         }
-
-        /// <summary>
-        /// Predicts the specified element.
-        /// </summary>
-        /// <param name="element">The element.</param>
-        /// <returns>predicted class</returns>
-        //public override float Predict(TProblemElement element)
-        //{
-        //    float sum = 0;
-
-        //    int index = -1;
-
-        //    for (int k = 0; k < TrainedModel.SupportElementsIndexes.Length; k++)
-        //    {
-        //        index = TrainedModel.SupportElementsIndexes[k];
-        //        sum += TrainedModel.Alpha[index] * TrainningProblem.Labels[index] *
-        //                            Kernel.Product(TrainningProblem.Elements[index], element);
-        //    }
-
-        //    sum -= TrainedModel.Rho;
-
-        //    float ret = sum > 0 ? 1 : -1;
-        //    return ret;
-        //}
     }
 }
